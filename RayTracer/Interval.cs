28c2264e7e7363f50ebb5ffdd5c5abce186828c6
Michal Šesták <lertcze@gmail.com>
2023-11-07 18:10:23 +0100

    Interval Class

---
+++ RayTracer/Interval.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracer
+{
+    internal class Interval
+    {
+        public double Min { get; }
+        public double Max { get; }
+
+        public static readonly Interval Empty = new Interval(double.PositiveInfinity, double.NegativeInfinity);
+        public static readonly Interval Universe = new Interval(double.NegativeInfinity, double.PositiveInfinity);
+
+        public Interval() : this(double.PositiveInfinity, double.NegativeInfinity)
+        {
+        }
+
+        public Interval(double min, double max)
+        {
+            Min = min;
+            Max = max;
+        }
+
+        public bool Contains(double x)
+        {
+            return Min <= x && x <= Max;
+        }
+
+        public bool Surrounds(double x)
+        {
+            return Min < x && x < Max;
+        }
+    }
+}
